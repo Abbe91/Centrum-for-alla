cd5340bb122e58df426155cb318eb4c3b1a4c21e
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2024-01-04 16:57:36 +0100

    Add the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import RandomUserButton from '../app/components/RandomUserButton';
+import Header from '../app/components/Header';
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Header />
       <RandomUserButton />
     </div>
   );
