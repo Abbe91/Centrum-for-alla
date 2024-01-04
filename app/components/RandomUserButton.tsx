afd0aef5f69ef98c1bcdd7a6e02863327614799f
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2024-01-04 13:51:35 +0100

    Create the component

---
+++ app/components/RandomUserButton.tsx
@@ -0,0 +1,21 @@
+// components/RandomUserButton.tsx
+"use client"
+import { useState } from 'react';
+
+const RandomUserButton: React.FC = () => {
+  
+
+  return (
+    <div>
+      
+     
+        <div>
+          <h2>User Information</h2>
+          <p>Name: </p>
+          <p>Email: </p>
+        </div>
+    </div>
+  );
+};
+
+export default RandomUserButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
+// pages/index.tsx
+import RandomUserButton from '../app/components/RandomUserButton';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-       
-      </div>
-    </main>
-  )
-}
+    <div>
+      <RandomUserButton />
+    </div>
+  );
+};
+
+export default Home;
