ecbba6970cb594cb93cf59640c2a73c9ffdbc707
abbe91 <abdullah.abo.alhaijja@hotmail.com>
2024-08-07 13:57:22 +0200

    add media query

--- src/components/MainComp/Divider.js
+++ src/components/MainComp/Divider.js
@@ -5,20 +5,21 @@ import "./Divider.css"; // Make sure to create this CSS file and import it
 const Divider = () => {
   return (
     <div className="divider-container">
-      <img
-        className="divider-image"
-        src={Marketing}
-        alt="marketing"
-        title="marketing"
-      />
-      <section>
-        <h2 className="divider-heading">Få hjälp av proffsen!</h2>
-        <p className="divider-paragraph">
-          Vårt team är välutbildat och erfaret, och vi arbetar snabbt och
-          effektivt för att underlätta för våra kunder under en tuff tid.
-        </p>
-      </section>
-    </div>
+    <img
+      className="divider-image"
+      src={Marketing}
+      alt="marketing"
+      title="marketing"
+    />
+    <section className="divider-text">
+      <h2 className="divider-heading">Få hjälp av proffsen!</h2>
+      <p className="divider-paragraph">
+        Vårt team är välutbildat och erfaret, och vi arbetar snabbt och
+        effektivt för att underlätta för våra kunder under en tuff tid.
+      </p>
+    </section>
+  </div>
+  
   );
 };
 
